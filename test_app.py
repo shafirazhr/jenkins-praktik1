339f546ce37368be16ef4383c4ee31018cbe4592
SHAFIRA ZUKHRUFATUZ ZAHRA <113220421+shafirazhr@users.noreply.github.com>
2025-05-06 22:10:04 +0700

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_hello():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
-    assert b"Hello from Jenkins Multibranch Pipeline!" in response.data
+    assert b"Hello from Jenkins Multibranch Pipeline" in response.data
